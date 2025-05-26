45fd53bd85522e0b034924ef988bf2c8287de9fb
Aeomar999 <amoahjerry835@gmail.com>
2025-05-26 16:33:50 +0000

    Modified the Home files and Title files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import hero_banner from '../../assets/hero_banner.jpg'
+import hero_banner from '../../assets/cards/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
+
 
 const Home = () => {
   return (
@@ -14,6 +18,11 @@ const Home = () => {
           <img src={hero_title} alt="" className='caption-img'/>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestias quibusdam sapiente quisquam fugiat,
              amet pariatur dolorum soluta cum esse ea sed numquam. Dolor voluptatibus eius fugit! Dignissimos, exercitationem omnis?</p>
+             <div className="hero-btns">
+              <button className='btn'><img src={play_icon} alt="" />Play</button>
+              <button className='btn dark-btn'><img src={info_icon} alt=""/>More Info</button>
+             </div>
+             <TitleCards/>
         </div>
       </div> 
     </div>
